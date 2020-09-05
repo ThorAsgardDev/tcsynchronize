b1f4f58c7556ff1251efe033ccd046ca445004ef
Thor <thor.asgard.dev@gmail.com>
2020-09-05 14:47:24 +0200

    update azure-pipelines

--- ci/package.cmd
+++ ci/package.cmd
@@ -8,7 +8,7 @@ mkdir "%ArtifactName%"
 
 xcopy "..\configuration" "%ArtifactName%\configuration" /E /Y /I /R
 xcopy "..\resources" "%ArtifactName%\resources" /E /Y /I /R
-xcopy ""..\bin\Release\netcoreapp3.1\win-x64\publish\TCSynchronize.exe" "%ArtifactName%\TCSynchronize.exe" /E /Y /I /R
+xcopy "..\bin\Release\netcoreapp3.1\win-x64\publish\TCSynchronize.exe" "%ArtifactName%" /E /Y /I /R
 
 REM Package ZIP archive
 7z a "%ArtifactName%.zip" "%ArtifactName%"
